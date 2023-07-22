e8fb59f858e5739abbf7b58f5bc40b52ffd2aab6
alyaa999 <alyaamamoon999@gmail.com>
2023-07-23 01:32:20 +0300

    add packages entity framework

---
+++ BookApp-API/myfirstapi/Migrations/20230722221647_testdatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myfirstapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class testdatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
