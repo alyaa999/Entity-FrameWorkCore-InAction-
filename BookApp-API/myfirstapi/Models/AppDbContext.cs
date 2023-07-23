3f97d5061cf88ecf70c9c950affa297eddd6935e
alyaa999 <alyaamamoon999@gmail.com>
2023-07-24 01:57:40 +0300

    add models

--- BookApp-API/myfirstapi/Models/AppDbContext.cs
+++ BookApp-API/myfirstapi/Models/AppDbContext.cs
@@ -1,5 +1,6 @@
 
 
+using DataLayer.EfClasses;
 using Microsoft.EntityFrameworkCore;
 
 class AppDbContext : DbContext {
@@ -8,6 +9,14 @@ class AppDbContext : DbContext {
  {
 
  }
- public DbSet<Book>? Books{ get; set;}
+              public DbSet<Book>  Books {get ; set;}  
+              public DbSet <BookAuthor> BookAuthors {get ; set;}
+
+              public DbSet <Author>  Authors  {get; set;}
+              public DbSet <PriceOffer> PriceOffers {get; set;}
+              public DbSet<Review>   Reviews {get; set;}
+              public DbSet <Tag>      tag  {get; set;} 
+
+
 
 }
